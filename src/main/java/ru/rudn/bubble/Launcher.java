c43137c3e1084045e3bbfd2975d6480e7a74b49e
Rexhaif <larionov.messerschmitt@gmail.com>
2016-12-07 18:39:03 +0300

    way 3

--- src/main/java/ru/rudn/bubble/Launcher.java
+++ src/main/java/ru/rudn/bubble/Launcher.java
@@ -7,5 +7,6 @@ public class Launcher {
     public static void main(String[] args){
         System.out.println("DryFear: Fuck the world!");
         System.out.println("Rexhaif: loli, you can't fuck it");
+        System.out.println("XXX");
     }
 }
